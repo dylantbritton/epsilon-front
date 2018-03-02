072f8814cc8a85f9abb61b773ee50260c89ee1cc
dylanbritton <dylantbritton@gmail.com>
2018-03-02 14:34:24 -0500

    mock database ready for test

--- epsilon/src/app/models/ability.model.ts
+++ epsilon/src/app/models/ability.model.ts
@@ -1,6 +1,6 @@
 import { abilityType } from "./abilityType.model";
 
-export class Ability{
+export class Ability {
 
     id: number;
     name: string;
@@ -8,7 +8,6 @@ export class Ability{
     cooldown: number;
     castTime: number;
     damage: number;
-    target: string;
     critChance: number;
     crit: number;
     effect: string;
@@ -16,22 +15,26 @@ export class Ability{
     speedExp: number;
     defenseExp: number;
     magicExp: number;
+    keyBinding: Number;
 
-    constructor(id, name, type, cooldown, castTime, damage, target, critChance, crit, effect, strengthExp, speedExp, defenseExp, magicExp){
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.cooldown = cooldown;
-        this.castTime = castTime;
-        this.damage = damage;
-        this.target = target;
-        this.critChance = critChance;
-        this.crit = crit;
-        this.effect = effect;
-        this.strengthExp = strengthExp;
-        this.speedExp = speedExp;
-        this.defenseExp = defenseExp;
-        this.magicExp = magicExp;
+
+    constructor($id: number, $name: string, $type: abilityType, $cooldown: number, $castTime: number, $damage: number,
+        $critChance: number, $crit: number, $effect: string, $strengthExp: number, $speedExp: number,
+        $defenseExp: number, $magicExp: number, $keyBinding: Number) {
+        this.id = $id;
+        this.name = $name;
+        this.type = $type;
+        this.cooldown = $cooldown;
+        this.castTime = $castTime;
+        this.damage = $damage;
+        this.critChance = $critChance;
+        this.crit = $crit;
+        this.effect = $effect;
+        this.strengthExp = $strengthExp;
+        this.speedExp = $speedExp;
+        this.defenseExp = $defenseExp;
+        this.magicExp = $magicExp;
+        this.keyBinding = $keyBinding;
     }
-
+    
 }
